bc908ef0881eafa33fa06301699fb7d645b9b345
Lulhoangminh <hoangminhbadao@gmail.com>
2022-04-10 16:03:49 +0700

    label con cac

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(180, 199);
+            this.label1.Location = new System.Drawing.Point(177, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(864, 177);
             this.label1.TabIndex = 0;
