6adad1356d730ed4298f00983323c9a4a118abd3
Lulhoangminh <hoangminhbadao@gmail.com>
2022-04-10 16:12:16 +0700

    ngu button

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.deptrai = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // deptrai
             // 
-            this.deptrai.Location = new System.Drawing.Point(109, 81);
+            this.deptrai.Location = new System.Drawing.Point(95, 61);
+            this.deptrai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deptrai.Name = "deptrai";
-            this.deptrai.Size = new System.Drawing.Size(88, 38);
+            this.deptrai.Size = new System.Drawing.Size(77, 28);
             this.deptrai.TabIndex = 0;
             this.deptrai.Text = "deptrai";
             this.deptrai.UseVisualStyleBackColor = true;
             this.deptrai.Click += new System.EventHandler(this.deptrai_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ngu";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.deptrai);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,5 +69,6 @@
         #endregion
 
         private Button deptrai;
+        private Button button1;
     }
 }
